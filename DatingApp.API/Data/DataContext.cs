1816ec59c9750898ab80dbc9df52598b9acdfe2f
Ivana S <ivanastankovski91@gmail.com>
2020-10-26 21:28:03 +0100

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        // prop to tell EF about our entities (Value model/entity in our case):
+        // convention pluralise the names of the entities value -> values(to be table name)      
+        public DbSet<Value> Values { get; set; }
+
+    }
+}
