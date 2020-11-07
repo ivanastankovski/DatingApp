583144e22a2d4418f40f07d30ec20b8ab6403693
Ivana S <ivanastankovski91@gmail.com>
2020-11-07 18:56:10 +0100

    Added login and register on the client side

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -8,7 +8,7 @@ namespace DatingApp.API.Dtos
         public string Username { get; set; }
 
         [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8 characters")]
+        [StringLength(32, MinimumLength = 8, ErrorMessage = "You must specify password between 4 and 8 characters")]
         public string Password { get; set; }
     }
 }
